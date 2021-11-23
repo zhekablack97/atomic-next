dbab75c16483822ec97b4ad566989e117d49b6d0
zhekablack97 <zhekablack97@yandex.ru>
2021-11-23 20:20:44 +0300

    пока все работает

---
+++ Atoms/Grid/src/index.ts
@@ -0,0 +1,17 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = void 0;
+
+var _react = _interopRequireDefault(require("react"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+const Grid = () => {
+  return /*#__PURE__*/_react.default.createElement(_react.default.Fragment, null, "mk,m,");
+};
+
+var _default = Grid;
+exports.default = _default;
